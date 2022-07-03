4c8f313f387ac593b20ee4ab4cb5359e7c96c0fd
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-07-03 13:41:15 +0100

    add long speech to text function

--- Main.py
+++ Main.py
@@ -1,7 +1,10 @@
 import speech_recognition as sr
+import os
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
 
 
-def speech_to_text(file="french-audio.wav"):
+def speech_to_text(file):
     recognizer = sr.Recognizer()
     text = ""
     with sr.AudioFile(file) as audio_file:
@@ -13,6 +16,26 @@ def speech_to_text(file="french-audio.wav"):
     return text
 
 
+def long_speech_to_text(file):
+    sound = AudioSegment.from_wav(file)
+    chunks = split_on_silence(
+        sound, min_silence_len=1000, silence_thresh=sound.dBFS-14, keep_silence=500)
+    folder_name = "chunks"
+    if not os.path.isdir(folder_name):
+        os.mkdir(folder_name)
+    text = ""
+    for i, audio_chunk in enumerate(chunks, start=1):
+        chunk_filename = os.path.join(folder_name, f"chunk{i}.wav")
+        audio_chunk.export(chunk_filename, format="wav")
+        text += speech_to_text(chunk_filename)
+    return text
+
+
 if __name__ == "__main__":
-    text = speech_to_text()
+    import time
+
+    start_time = time.time()
+    FILE = "audio/french-film-podcast.wav" #insert the path of an audio file here
+    text = long_speech_to_text(FILE)
     print(text)
+    print(f"----- The program took {time.time() - start_time} seconds to finish -----")
